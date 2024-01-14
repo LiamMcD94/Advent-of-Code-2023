f8f3c513ac4ad0d00ab65b727d497bdfde39c27a
LiamMcD94 <lamcd94@hotmail.com>
2024-01-14 23:47:41 +0000

    Corrected the wrong use of Y and X co-ords
    use of X and Y where accidentally flipped. corrected

--- Day 10/Day10.py
+++ Day 10/Day10.py
@@ -18,9 +18,9 @@ def part1():
 
     def pipeTravel():
         line, position = findStartReturn
-        Y = position
-        X = line
-        inputLines[X][Y]
+        X = position
+        Y = line
+        inputLines[Y][X]
 
         endPosition = ""
         lastPosition = ""
@@ -28,124 +28,124 @@ def part1():
 
         while endPosition != "S":
             #check up the position
-            if inputLines[X][Y] in ('S'):
-                if inputLines[X - 1][Y] in ("|", "7", "F"):
-                #  print("there is an {} above starting postion, going up!".format(inputLines[up][Y]))
-                    X -= 1
+            if inputLines[Y][X] in ('S'):
+                if inputLines[Y - 1][X] in ("|", "7", "F"):
+                #  print("there is an {} above starting postion, going up!".format(inputLines[Y += 1][X]))
+                    Y -= 1
                     lastPosition = "up"
-                    endPosition = inputLines[X][Y]
+                    endPosition = inputLines[Y][X]
                     steps += 1
 
-                elif inputLines[line][Y - 1] in ("-" or "F" or "L") and lastPosition != "right":
-                #  print("there is an {} left of the starting postion, going left!".format(inputLines[X][left]))
-                    Y -= 1
-                    endPosition = inputLines[X][Y]
+                elif inputLines[line][X - 1] in ("-" or "F" or "L") and lastPosition != "right":
+                #  print("there is an {} left of the starting postion, going left!".format(inputLines[Y][X -= 1]))
+                    X -= 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "left"
                     steps += 1
 
                 #check to the right of position
-                elif inputLines[line][Y + 1] in ("-" or "7" or "J") and lastPosition != "left":
-                #  print("there is an {} right of the starting postion, going left!".format(inputLines[X][right]))  
-                    Y += 1
-                    endPosition = inputLines[X][Y]
+                elif inputLines[line][X + 1] in ("-" or "7" or "J") and lastPosition != "left":
+                #  print("there is an {} right of the starting postion, going left!".format(inputLines[Y][X += 1]))  
+                    X += 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "right"
                     steps += 1
             
                 #check down position
-                elif inputLines[X + 1][position] in ("|" or "L" or "J") and lastPosition != "up":
-                #  print("there is an {} below the starting postion, going down!".format(inputLines[down][Y]))
-                    X += 1
-                    endPosition = inputLines[X][Y]
+                elif inputLines[Y + 1][X] in ("|" or "L" or "J") and lastPosition != "up":
+                #  print("there is an {} below the starting postion, going down!".format(inputLines[Y += 1][X]))
+                    Y += 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "down"
                     steps += 1
 
-            elif inputLines[X][Y] in ('F'):
+            elif inputLines[Y][X] in ('F'):
                 if lastPosition == "up":
-                ##  print("its a {}, going right!".format(inputLines[X][Y]))
-                    Y += 1
-                    endPosition = inputLines[X][Y]
+                ##  print("its a {}, going right!".format(inputLines[Y][X]))
+                    X += 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "right"
                     steps += 1
 
                 elif lastPosition == "left":
-                # print("it's a {}, going down!".format(inputLines[X][Y]))
-                    X += 1
-                    endPosition = inputLines[X][Y]
+                # print("it's a {}, going down!".format(inputLines[Y][X]))
+                    Y += 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "down"
                     steps += 1
 
-            elif inputLines[X][Y] in ('J'):
+            elif inputLines[Y][X] in ('J'):
                 if lastPosition == "right":
-                    #print("it's a {}, going up!{}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    X -= 1
-                    endPosition = inputLines[X][Y]
+                    #print("it's a {}, going up!{}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    Y -= 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "up"
                     steps += 1
 
                 elif lastPosition == "down":
-                # print("it's a {}, going left!{}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    Y -= 1
-                    endPosition = inputLines[X][Y]
+                # print("it's a {}, going left!{}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    X -= 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "left"
                     steps += 1
             
-            elif inputLines[X][Y] in ('L'):
+            elif inputLines[Y][X] in ('L'):
                 if lastPosition == "left":
-                    #print("it's a {}, going up!{}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    X -= 1
-                    endPosition = inputLines[X][Y]
+                    #print("it's a {}, going up!{}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    Y -= 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "up"
                     steps += 1
 
                 elif lastPosition == "down":
-                    #print("it's a {}, going right! position {}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    Y += 1
-                    endPosition = inputLines[X][Y]
+                    #print("it's a {}, going right! position {}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    X += 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "right"
                     steps += 1
 
-            elif inputLines[X][Y] in ('|'):
+            elif inputLines[Y][X] in ('|'):
                 if lastPosition == "up":
-                    #print("it's a {}, going up!{}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    X -= 1
-                    endPosition = inputLines[X][Y]
+                    #print("it's a {}, going up!{}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    Y -= 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "up"
                     steps += 1
 
                 elif lastPosition == "down":
-                    #print("it's a {}, going down!{}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    X += 1
-                    endPosition = inputLines[X][Y]
+                    #print("it's a {}, going down!{}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    Y += 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "down"
                     steps += 1
                 
-            elif inputLines[X][Y] in ('-'):
+            elif inputLines[Y][X] in ('-'):
                 if lastPosition == "left":
-                    #print("it's a {}, going left!{}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    Y -= 1
-                    endPosition = inputLines[X][Y]
+                    #print("it's a {}, going left!{}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    X -= 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "left"
                     steps += 1
 
                 elif lastPosition == "right":
-                    #print("it's a {}, going right!{}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    Y += 1
-                    endPosition = inputLines[X][Y]
+                    #print("it's a {}, going right!{}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    X += 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "right"
                     steps += 1
 
-            elif inputLines[X][Y] in ('7'):
+            elif inputLines[Y][X] in ('7'):
                 if lastPosition == "up":
-                    #print("it's a {}, going left!{}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    Y -= 1
-                    endPosition = inputLines[X][Y]
+                    #print("it's a {}, going left!{}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    X -= 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "left"
                     steps += 1
 
                 elif lastPosition == "right":
-                    #print("it's a {}, going down!{}, {} and last position {}".format(inputLines[X][Y], [X], [Y], endPosition))
-                    X += 1
-                    endPosition = inputLines[X][Y]
+                    #print("it's a {}, going down!{}, {} and last position {}".format(inputLines[Y][X], [Y], [X], endPosition))
+                    Y += 1
+                    endPosition = inputLines[Y][X]
                     lastPosition = "down"
                     steps += 1      
         result = steps /2
